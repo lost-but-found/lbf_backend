f2aa181f3e35dc7717a40c71f0fdb2af880b0c93
Prince Codes <princecodes247@gmail.com>
2023-08-18 23:52:45 +0100

    remove build

--- build/routes/refresh.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require('express');
-const router = express.Router();
-const refreshTokenController = require('../controllers/refreshTokenController');
-router.get('/', refreshTokenController.handleRefreshToken);
-module.exports = router;
-//# sourceMappingURL=refresh.js.map
