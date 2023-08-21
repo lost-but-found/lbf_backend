ddd4c36b540c716ee2e8b80d704fe4dc476910be
Prince Codes <princecodes247@gmail.com>
2023-08-21 17:39:28 +0100

    fix breaking routes

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -7,7 +7,7 @@ const AuthRouter: Router = express.Router();
 AuthRouter.post("/", AuthController.login);
 AuthRouter.post("/", AuthController.logout);
 AuthRouter.get("/", AuthController.refreshToken);
-AuthRouter.post("/", upload.single("photo"), AuthController.register);
+AuthRouter.post("/register", upload.single("photo"), AuthController.register);
 AuthRouter.post("/", AuthController.sendOTPToUser);
 AuthRouter.post("/", AuthController.resendOTP);
 AuthRouter.post("/", AuthController.verifyOTP);
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import path from "path";
 import { Application, Router } from "express";
 import { AuthRouter } from "./modules/auth";
@@ -10,8 +10,8 @@ const router: Router = Router();
 
 router.use("/auth", AuthRouter);
 router.use("/category", CategoryRouter);
-router.use("/item", ItemRouter);
-router.use("/user", UserRouter);
+router.use("/items", ItemRouter);
+router.use("/users", UserRouter);
 
 const routes = (app: Application) => {
   app.use("/api/v1", router);
@@ -22,6 +22,21 @@ const routes = (app: Application) => {
     "/profile-image",
     express.static(path.join(__dirname, "profile-image"))
   );
+
+  // Error handler for 404 - Page Not Found
+  app.use((req: Request, res: Response, next) => {
+    console.log("---- 404 error handler", req.originalUrl);
+    res.status(404).json({ message: "Sorry, page not found!" });
+  });
+
+  app.use((err, req, res, next) => {
+    if (err) {
+      res.status(500).json({
+        message: err.message,
+      });
+      next();
+    }
+  });
 };
 
 export default routes;
