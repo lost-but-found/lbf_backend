2e4059332f528edb97ec43476294acf52e0701f7
Prince Codes <princecodes247@gmail.com>
2023-08-23 21:54:02 +0100

    implement sendResponse

--- src/modules/category/category.controller.ts
+++ src/modules/category/category.controller.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from "express";
 import Category from "./category.model";
+import { sendResponse } from "../../utils/sendResponse";
+import { StatusCodes } from "http-status-codes";
 
 const getAllCategories = async (req: Request, res: Response) => {
-  const allCategories = await Category.find({});
-  res.status(200).json(allCategories);
+  try {
+    const allCategories = await Category.find({});
+
+    return sendResponse({
+      res,
+      status: StatusCodes.OK,
+      message: "All categories fetched successfully.",
+      data: allCategories,
+      success: true,
+    });
+  } catch (err) {
+    console.error(err);
+    return sendResponse({
+      res,
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
+      message: "Failed to fetch categories.",
+      success: false,
+    });
+  }
 };
 
 // Route for adding a category
@@ -14,7 +33,12 @@ const addCategory = async (req: Request, res: Response) => {
 
     // Perform any necessary validation on the data
     if (!name) {
-      return res.status(400).json({ error: "Category name is required." });
+      return sendResponse({
+        res,
+        status: StatusCodes.BAD_REQUEST,
+        message: "Category name is required.",
+        success: false,
+      });
     }
 
     // Create a new Category instance
@@ -26,17 +50,30 @@ const addCategory = async (req: Request, res: Response) => {
     // Check if the category name already exists
     const existingCategory = await Category.findOne({ name });
     if (existingCategory) {
-      return res.status(400).json({ error: "Category name already exists." });
+      return sendResponse({
+        res,
+        status: StatusCodes.BAD_REQUEST,
+        message: "Category name already exists.",
+        success: false,
+      });
     }
 
     // Return a success response
-    return res.status(201).json({
+    return sendResponse({
+      res,
+      status: StatusCodes.CREATED,
       message: "Category created successfully.",
-      category: newCategory,
+      data: newCategory,
+      success: true,
     });
   } catch (err) {
     console.error(err);
-    return res.status(500).json({ error: "Failed to create the category." });
+    return sendResponse({
+      res,
+      status: StatusCodes.INTERNAL_SERVER_ERROR,
+      message: "Failed to create category.",
+      success: false,
+    });
   }
 };
 
