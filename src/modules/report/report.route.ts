a5158c82d9096037e6aa99b5e24f637d82f68e3d
Prince Codes <princecodes247@gmail.com>
2023-10-04 19:30:42 +0100

    require auth for report

--- src/modules/report/report.route.ts
+++ src/modules/report/report.route.ts
@@ -2,14 +2,16 @@ import express from "express";
 import ReportController from "./report.controller";
 import validateRequest from "../../middleware/validateRequest";
 import { makeReportSchema } from "./schemas/make-report.schema";
+import { isAuth } from "../auth/auth.middleware";
 
 const router = express.Router();
 
 router.post(
   "/",
+  isAuth,
   validateRequest(makeReportSchema),
   ReportController.createReport
 );
-router.get("/", ReportController.getReports);
+router.get("/", isAuth, ReportController.getReports);
 
 export default router;
